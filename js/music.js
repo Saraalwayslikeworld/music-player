d6c7953e34ef2d367b1be3eb23882b5a91cac546
你的名称 <email>
2018-03-15 15:42:07 +0800

    json更改

--- js/music.js
+++ js/music.js
@@ -1,1 +1,22 @@
-var 
+
+function getMusic(callback){
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET','../music-player/music.json',true)
+    xhr.send()
+    xhr.onload = function(req,res){
+        if((xhr.status>=200 && xhr.status<300)||xhr.status===304){
+            callback(JSON.parse(this.responseText))
+        }else{
+            res.end('<h1>服务器出错</h1>')
+        }
+    }
+    xhr.onerror = function(){
+        console.log('error')
+    }
+}
+
+getMusic(function(list){
+    var song = list[0]
+    var audioObject = new audioObject(song.src)
+    play(audioObject)
+})
